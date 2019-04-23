35a2e7ce3657b45fa82cf9b91e8f4177fc46a070
davichodavid <davichodavid@yahoo.com>
2019-04-23 15:59:17 -0400

    Pull request for Calculator project

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = () => {
+  return ("yup still here");
+}
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Button.css';
+
+const NumArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const NumberButton = () => {
+  return (
+    NumArray.map(num => {
+      return <button className={props.buttonStyle}> {props.text} </button>
+    })
+  );
+}
+
+export default NumberButton;
