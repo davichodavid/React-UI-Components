5b09e2faedde006a50544258f1d5efda91ca8f10
davichodavid <davichodavid@yahoo.com>
2019-04-23 21:28:15 -0400

    Created the arrays in app.js and passed them down

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -7,7 +7,7 @@ import ZeroButton from './components/ButtonComponents/ZeroButton';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 import Operators from './components/ButtonComponents/Operators'
 
-// const NumArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", ];
+const NumArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9",];
 const action = ["Clear", "0"];
 
 const App = () => {
@@ -18,7 +18,7 @@ const App = () => {
       <div className="numbers-operators">
         <div>
           <ActionButton clearButton={action} />
-          <NumberButton />
+          <NumberButton numberButton={NumArray} />
           <ZeroButton zeroButton={action} />
         </div>
         <Operators />
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Button.css';
 
-const NumArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const NumArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const NumberButton = () => {
+const NumberButton = (props) => {
   return (
     <div className="number-container">
-      {NumArray.map(num => {
+      {props.numberButton.map(num => {
         return <button className="button-styles">{num}</button>
       })}
     </div>
